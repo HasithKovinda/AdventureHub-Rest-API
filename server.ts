de2eab741e505039a30ff460e2ee089558068efb
HasithKovinda <hasithkovinda@outlook.com>
2024-04-21 14:37:37 +0530

    add configration files for differnet env

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
-import express from "express";
 import app from "./app";
+import config from "config";
 
-app.listen(3000, () => {
-  console.log("Server Listen On Port 3000");
+const port = config.get<number>("port") || 8000;
+const env = app.get("env");
+console.log(process.env);
+app.listen(port, () => {
+  console.log(`Server Listen On Port ${port}`);
 });
