c1ce312cf8c933cd4db797b0defe9d5b401b1986
HasithKovinda <hasithkovinda@outlook.com>
2024-04-21 20:16:15 +0530

    add development database connection

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,4 @@
 export default {
   port: 3000,
+  connection_string: "mongodb://localhost:27017/adventure",
 };
--- server.ts
+++ server.ts
@@ -1,9 +1,20 @@
 import app from "./app";
 import config from "config";
+import mongoose from "mongoose";
 
 const port = config.get<number>("port") || 8000;
-const env = app.get("env");
-console.log(process.env);
+
+const connectString = config.get<string>("connection_string");
+
+mongoose
+  .connect(connectString)
+  .then(() => {
+    console.log("Db connection successful ✨");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 app.listen(port, () => {
-  console.log(`Server Listen On Port ${port}`);
+  console.log(`Server Listen On Port ${port} ✔`);
 });
