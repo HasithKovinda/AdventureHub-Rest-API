f8b8e2b612c290b3202450b84a648a868df69e27
HasithKovinda <hasithkovinda@outlook.com>
2024-04-30 23:44:36 +0530

    prevent parameter pollution

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
 import tourRoute from "./src/routes/tour.route";
 import userRoute from "./src/routes/user.route";
 import AppError from "./src/util/AppError";
@@ -11,10 +13,15 @@ const app = express();
 
 app.use(morgan("dev"));
 
+//Set HTTP response headers
 app.use(helmet());
 
 app.use(express.json());
 
+//Prevent NoSql query injection attack
+app.use(mongoSanitize());
+
+//Limit Incoming requests for an hour
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   limit: 100,
@@ -23,15 +30,32 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "difficulty",
+      "price",
+      "maxGroupSize",
+      "duration",
+      "ratingsAverage",
+      "ratingsQuantity",
+    ],
+  })
+);
+
+//Routes
 app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/user", userRoute);
 
+//Handle not define routes
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
   next(
     new AppError(`Can't find this route ${req.originalUrl} on this server`, 404)
   );
 });
 
+//Global error middleware
 app.use(globalErrorHandler);
 
 export default app;
