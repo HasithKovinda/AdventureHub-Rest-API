33fc742319b3dec371ae18db8c21d820aef4e3ac
HasithKovinda <hasithkovinda@outlook.com>
2024-05-02 22:37:51 +0530

    implement review model

---
+++ src/models/review.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Document } from "mongoose";
+import { TourDocument } from "./tour.model";
+import { UserDocument } from "./user.model";
+
+export interface ReviewInput {
+  review: string;
+  rating: number;
+  tour: TourDocument["id"];
+  user: UserDocument["id"];
+}
+
+export interface ReviewDocument extends ReviewInput, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      trim: true,
+      required: [true, "Review can not be empty"],
+    },
+    rating: {
+      type: Number,
+      required: [true, "Review should have a rating"],
+      min: [1, "A rating should have a value above 1"],
+      max: [5, "A rating should have a value bellow 6"],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+const Review = mongoose.model<ReviewDocument>("Review", ReviewSchema);
+
+export default Review;
