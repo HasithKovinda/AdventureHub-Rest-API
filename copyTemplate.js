16fe9474cc58972bf92b98daf34af486d60ca2af
HasithKovinda <hasithkovinda@outlook.com>
2024-05-08 18:17:29 +0530

    fix  sytax issue in copy method

--- copyTemplate.js
+++ copyTemplate.js
@@ -1,9 +1,9 @@
 const fs = require("fs-extra");
 
-fs.copy(`${__dirname}/src/templates`, `${__dirname}/build/templates`);
+fs.copy(`${__dirname}/src/templates`, `${__dirname}/build/templates`)
   .then(() => {
     console.log(`${__dirname}/src/templates`);
-    console.log(`${__dirname}/build/templates`)
+    console.log(`${__dirname}/build/templates`);
     console.log("Templates copied successfully!");
   })
   .catch((err) => {
